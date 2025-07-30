5206d9c4f79dc2cb2c6f1e0fa5dd5ab999bd522d
leonardogonzalolaura <glleonardodaniel@gmail.com>
2025-07-30 17:45:00 -0500

    update config next

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+
+const nextConfig = {
+  output: 'export', // Habilita el modo estático (requerido para GitHub Pages)
+  basePath: process.env.NODE_ENV === 'production' ? '/atechlofact' : '', // Cambia 'atechlofact' por tu nombre de repo
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/atechlofact/' : '', // Mismo nombre aquí
 };
 
-export default nextConfig;
+module.exports = nextConfig;
