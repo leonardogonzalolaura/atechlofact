9af27c0848b93ce13878ec80d4116ed5206234b6
leonardogonzalolaura <glleonardodaniel@gmail.com>
2025-07-30 17:51:33 -0500

    enable igonre

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 
 const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true, // 👈 Ignora errores de ESLint durante el build
+  },
   output: 'export', // Habilita el modo estático (requerido para GitHub Pages)
   basePath: process.env.NODE_ENV === 'production' ? '/atechlofact' : '', // Cambia 'atechlofact' por tu nombre de repo
   assetPrefix: process.env.NODE_ENV === 'production' ? '/atechlofact/' : '', // Mismo nombre aquí
