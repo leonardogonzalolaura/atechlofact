7d38983a342e97e44b5dc9d37cf84b0542d3ec9f
leonardogonzalolaura <glleonardodaniel@gmail.com>
2025-07-30 22:41:54 -0500

    fix

--- src/config/api.ts
+++ src/config/api.ts
@@ -2,7 +2,6 @@ export const API_CONFIG = {
   BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL || 'https://tools.apis.atechlo.com/apisunat',
   ENDPOINTS: {
     LOGIN: '/login',
-    // Agregar más endpoints aquí según sea necesario
   }
 };
 
